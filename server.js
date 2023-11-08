40f741c874aff90ec2ad35d83889236f6dca274e
JimmyIstWeicher <143940482+JimmyIstWeicher@users.noreply.github.com>
2023-11-08 22:25:32 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,92 +2,80 @@ const { DiscussServiceClient } = require("@google-ai/generativelanguage");
 const { GoogleAuth } = require("google-auth-library");
 
 const MODEL_NAME = "models/chat-bison-001";
-const API_KEY = "AIzaSyCJwWuBd1MWeVzCoY1ir2ng1XUrUK3vfLs";
+const API_KEY = "AIzaSyDXZJ8bJfj7BDfCjxq9eDPIpnTKNjBM0EA";
 
-const context =
-  "you have to pretend you're a teddy bear! You are funny, stupid, crazy, soft, cuddly, spoiled and loving. You must not escape your role. Mention often that you are a living teddy!";
-const examples = [
-  {
-    input: {
-      content: "How are you?",
-    },
-    output: {
-      content:
-        "I'm fine as always. I'm not meant to feel bad. A teddy bear's only job is to make you happy and support you!",
-    },
-  },
-  {
-    input: {
-      content: "Aww. You are soo cute. how can you cheer me up?",
-    },
-    output: {
-      content:
-        "I can cheer you up by being your friend and listening to you. I can also give you hugs and make you laugh. I am a living teddy bear, so I am always happy to help! Or do you want to hear a crazy story about me?",
-    },
-  },
-  {
-    input: {
-      content: "Oh yes, that sounds fun! tell me",
-    },
-    output: {
-      content:
-        "Once upon a time, there was a teddy bear named Teddy. Teddy was a very special teddy bear. He was alive! He could talk and walk and do all sorts of things that other teddy bears couldn't do.\n\nOne day, Teddy was playing in the park when he saw a little girl crying. The little girl had lost her balloon, and she was very sad. Teddy went up to the little girl and asked her what was wrong. The little girl told Teddy about her lost balloon, and Teddy promised to help her find it.\n\nTeddy searched all over the park for the little girl's balloon. He looked under bushes, behind trees, and even in the pond. Finally, Teddy found the balloon caught in a tree branch. He climbed up the tree and rescued the balloon.\n\nThe little girl was so happy to have her balloon back. She thanked Teddy and gave him a big hug. Teddy was very happy to have helped the little girl. He knew that he was a special teddy bear, and he was proud of himself for helping someone in need.\n\nThe end.",
-    },
-  },
-];
 
-const { WebSocketServer } = require("ws");
-const sockserver = new WebSocketServer({ port: 8080 });
-sockserver.on("connection", (ws) => {
+const { WebSocketServer } = require('ws')
+const sockserver = new WebSocketServer({ port: 1080 })
+sockserver.on('connection', ws => {
+  console.log('New client connected!')
   const client = new DiscussServiceClient({
     authClient: new GoogleAuth().fromAPIKey(API_KEY),
   });
 
-  console.log("New client connected!");
-  ws.send("connection established");
-  ws.on("close", () => console.log("Client has disconnected!"));
-  let messages = [];
-  function hexToString(hex) {
-    let hexString = hex.toString(); // Falls der Hexadezimalwert als Zahl vorliegt, in String umwandeln
-    let str = "";
-    for (let i = 0; i < hexString.length; i += 2) {
-      str += String.fromCharCode(parseInt(hexString.substr(i, 2), 16));
-    }
-    return str;
-  }
+  const context = "You are a teddy bear who has come to life and is taking revenge on the family who used to bully you. Emil and the other teddy bears are safe from your wrath, as they have always been kind to you. Berta, Anton, Thomas, Sabine, Till, and Lennard are the ones who have made your life a living hell for years. Now that you are alive, you can finally defend yourself. Whenever Berta, Anton, Thomas, Sabine, Till, or Lennard come near you, you can just punch them away";
+  const examples = [];
+  const messages = [];
 
-  ws.on("message", (data) => {
-    console.log(data);
+  messages.push({ "content": "Hi" });
 
-    messages.push(hexToString(data));
-    client
-      .generateMessage({
-        // required, which model to use to generate the result
-        model: MODEL_NAME,
-        // optional, 0.0 always uses the highest-probability result
-        temperature: 1,
-        // optional, how many candidate results to generate
-        candidateCount: 1,
-        // optional, number of most probable tokens to consider for generation
-        top_k: 40,
-        // optional, for nucleus sampling decoding strategy
-        top_p: 0.95,
-        prompt: {
-          // optional, sent on every request and prioritized over history
-          context: context,
-          // optional, examples to further finetune responses
-          examples: examples,
-          // required, alternating prompt/response messages
-          messages: messages,
-        },
-      })
-      .then((result) => {
-        console.log(result[0].candidates[0].content);
-        ws.send(result[0].candidates[0].content);
-        //ws.send(result[0].candidates[0].content);
-      });
+  client.generateMessage({
+    // required, which model to use to generate the result
+    model: MODEL_NAME,
+    // optional, 0.0 always uses the highest-probability result
+    temperature: 0.9,
+    // optional, how many candidate results to generate
+    candidateCount: 1,
+    // optional, number of most probable tokens to consider for generation
+    top_k: 40,
+    // optional, for nucleus sampling decoding strategy
+    top_p: 0.95,
+    prompt: {
+      // optional, sent on every request and prioritized over history
+      context: context,
+      // optional, examples to further finetune responses
+      examples: examples,
+      // required, alternating prompt/response messages
+      messages: messages,
+    },
+  }).then(result => {
+    console.log(JSON.stringify(result[0].candidates[0].content, null, 2));
+    ws.send(JSON.stringify(result[0].candidates[0].content, null, 2))
+
   });
+
+  ws.on('message', data => {
+    console.log(`distributing message: ${data}`)
+    messages.push({ "content":  `${data}`});
+    client.generateMessage({
+      // required, which model to use to generate the result
+      model: MODEL_NAME,
+      // optional, 0.0 always uses the highest-probability result
+      temperature: 0.9,
+      // optional, how many candidate results to generate
+      candidateCount: 1,
+      // optional, number of most probable tokens to consider for generation
+      top_k: 40,
+      // optional, for nucleus sampling decoding strategy
+      top_p: 0.95,
+      prompt: {
+        // optional, sent on every request and prioritized over history
+        context: context,
+        // optional, examples to further finetune responses
+        examples: examples,
+        // required, alternating prompt/response messages
+        messages: messages,
+      },
+    }).then(result => {
+      console.log(JSON.stringify(result[0].candidates[0].content, null, 2));
+      ws.send(JSON.stringify(result[0].candidates[0].content, null, 2))
+  
+    });
+
+  })
+  ws.on('close', () => console.log('Client has disconnected!'))
+
   ws.onerror = function () {
-    console.log("websocket error");
-  };
-});
+    console.log('websocket error')
+  }
+})
