2a4edc40b97cd89a581b92ae63b743c75291cd2a
JimmyIstWeicher <143940482+JimmyIstWeicher@users.noreply.github.com>
2023-12-16 15:39:00 +0100

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ async function runChat(user) {
   const model = genAI.getGenerativeModel({ model: MODEL_NAME });
 
   const generationConfig = {
-    temperature: 0.9,
+    temperature: 0.8,
     topK: 1,
     topP: 1,
     maxOutputTokens: 2048,
