e7af4cece38a858219b332dae2e655bece75439e
JimmyIstWeicher <143940482+JimmyIstWeicher@users.noreply.github.com>
2023-12-18 22:45:21 +0100

    Update server.js

--- server.js
+++ server.js
@@ -307,7 +307,7 @@ async function runChat(user) {
   });
 }
 
-const sockserver = new WebSocketServer({ port: 8080 });
+const sockserver = new WebSocketServer({ port: 3000 });
 console.log("ists running");
 sockserver.on("connection", (ws) => {
   console.log("New client connected!");
